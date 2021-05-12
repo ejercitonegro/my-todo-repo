da77b08a23e640f064e47d9ddd7662a8901730ed
Pablo <pablorevueltabriz@gmail.com>
2021-05-12 14:41:47 +0000

    debug

--- todos/translate_en.py
+++ todos/translate_en.py
@@ -7,20 +7,32 @@ import boto3
 translate = boto3.client('translate')
 
 def translate_en(event, context):
-    result = get.get(event, context)
+    #result = get.get(event, context)
 
-    dict_result = json.loads(result)
+    json_string = """
+    [
+        {
+            "checked": false,
+            "createdAt": "1620823279.6609614",
+            "text": "texto en español",
+            "id": "59c8f383-b31f-11eb-9179-a5009fe4c60e",
+            "updatedAt": "1620823279.6609614"
+        }
+    ]
+    """
 
-    idValue = dict_result["id"]
+    result = json.loads(result)
 
-    idValue_translated = translate.translate_text(Text=idValue, SourceLanguageCode="es", TargetLanguageCode="en")
+    #idValue = dict_result["id"]
 
-    dict_result['id'] = idValue_translated
+    #idValue_translated = translate.translate_text(Text=idValue, SourceLanguageCode="es", TargetLanguageCode="en")
+
+    #dict_result['id'] = idValue_translated
 
     # create a response
     response = {
             "statusCode": 200,
-            "body": json.dumps(dict_result['Item'],
+            "body": json.dumps(result['Item'],
                                 cls=decimalencoder.DecimalEncoder)
     }
 
