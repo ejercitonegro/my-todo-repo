91606d6e77c5178d07fdce7d95eaca28186f6bc5
Pablo <pablorevueltabriz@gmail.com>
2021-05-12 13:04:35 +0000

    debug

--- todos/translate_en.py
+++ todos/translate_en.py
@@ -6,7 +6,11 @@ import boto3
 
 translate = boto3.client('translate')
 
-
 def translate_en(event, context):
-    result=get.get(event, context)
-    return result
+    result = get.get(event, context)
+
+    idValue = result['id']
+    #result_translated = translate.translate_text(Text=result, SourceLanguageCode="es", TargetLanguageCode="en")
+
+    #return result_translated
+    return idValue
