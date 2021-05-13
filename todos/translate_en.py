852ab2788bb314191a7fdeb0f450930c9337db11
Pablo <pablorevueltabriz@gmail.com>
2021-05-13 12:09:38 +0000

    debug

--- todos/translate_en.py
+++ todos/translate_en.py
@@ -1,5 +1,6 @@
 import os
 import json
+import codecs
 
 from todos import get
 import boto3
@@ -8,5 +9,16 @@ translate = boto3.client('translate')
 
 def translate_en(event, context):
     result = get.get(event, context)
-    
+
+    with codecs.open("result.json", 'r', 'utf8') as data_file:
+        result = json.load(data_file)
+
+    for i in result:
+        for key in i:
+            if key=="text":
+                value = i[key]
+                translated = translate.translate_text(Text=value, SourceLanguageCode='auto', TargetLanguageCode='es')
+                i[key] = translated
+
+
     return result
