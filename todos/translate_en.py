7ff66031e65bf2667630d52d740c1d865786d392
Pablo <pablorevueltabriz@gmail.com>
2021-05-13 12:45:51 +0000

    debug

--- todos/translate_en.py
+++ todos/translate_en.py
@@ -2,13 +2,21 @@ import os
 import json
 import codecs
 
-from todos import get
+#from todos import get
 import boto3
 
 translate = boto3.client('translate')
 
 def translate_en(event, context):
-    result = get.get(event, context)
+    #result = get.get(event, context)
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+
+    # fetch todo from the database
+    result = table.get_item(
+        Key={
+           'id': event['pathParameters']['id']
+        }
+    )
 
     for i in result:
         for key in i:
